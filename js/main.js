696c9662c7142809df4c8b40f78c8d85f01d1246
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-10-18 16:09:58 +0900

    카드까진 만들었다
    이제 다음엔 카드 배치를 하고, 카드에 영화 사진을 넣고, 카드를 누르면 id가 출력되게 해야겠지? 하나씩 ㄱㄱ

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dataContainer = document.getElementById("data-container");
-  const fetchDataButton = document.getElementById("fetch-data");
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzUxODEzMGMyNmQyNDk3MTQzMzI0ZDE2ZmQ5ZmRjMiIsInN1YiI6IjY1MmYzMGEzZWE4NGM3MDBjYTEyYWYzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MDJteH71TG0WQ7joW6cLBTmEwqEvkwDjud9DOqQ3WnQ",
+    },
+  };
 
-  fetchDataButton.addEventListener("click", function () {
-    // 여기에 외부 API를 호출하고 데이터를 가져오는 코드를 작성하세요.
-    // 아래는 제공한 코드를 기반으로 작성된 예시입니다.
+  fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const dataContainer = document.getElementById("data-container");
+      let movies = response.results;
+      console.log(movies);
+      movies.forEach((mov) => {
+        const card = createMovieCard(mov);
+        dataContainer.appendChild(card);
+        // let title = mov["title"];
+        // let overview = mov["overview"];
+        // console.log("[제목] : " + title + " [요약] :" + overview);
 
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzUxODEzMGMyNmD2NDk3MTQzMzI0ZDE2ZmQ5ZmRjMiIsInN1YiI6IjY1MmYzMGEzZWE4NGM3MDBjYTEyYWYzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MDJteH71TG0WQ7joW6cLBTmEwqEvkwDjud9DOqQ3WnQ",
-      },
-    };
+        function createMovieCard(mov) {
+          // 카드 엘리먼트를 생성합니다.
+          const card = document.createElement("div");
+          card.classList.add("movie-card");
 
-    fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-      options
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // 가져온 데이터를 화면에 표시합니다.
-        displayData(data);
-      })
-      .catch((err) => {
-        console.error("데이터 가져오기 실패:", err);
+          // 카드 내용을 구성합니다.
+          const title = document.createElement("h2");
+          title.textContent = mov["title"];
+
+          const overview = document.createElement("p");
+          overview.textContent = mov["overview"];
+
+          const voteAverage = document.createElement("p");
+          voteAverage.textContent = "평균 평점: " + mov["vote_average"];
+
+          // 카드에 내용을 추가합니다.
+          card.appendChild(title);
+          card.appendChild(overview);
+          card.appendChild(voteAverage);
+
+          return card;
+        }
       });
-  });
-
-  function displayData(data) {
-    // 데이터를 출력할 방식을 정의합니다.
-    // 이 예시에서는 데이터를 JSON 문자열로 출력합니다.
-    dataContainer.innerHTML = JSON.stringify(data, null, 2);
-  }
+    })
+    .catch((err) => console.error(err));
 });
+//좋아좋아 여기까진 좋아. 웹페이지가 로드 되자마자 fetch로 자료를 받아오고 그걸 .json() 으로 js 객체 만든 다음에
+//콘솔에 찍는것까지 성공했어! 이제 뭐 어떡하지...?
+//카드에 들어가야 하는 내용 title, overview, vote_average, 그리고 클릭하면 id 가 나오게 해야해.
+// 카드로 만들고 자시고 일단 저걸 내가 다룰수 있는 객체로 만들고 싶은데 어떡해야할까?
+
+//오케이 response에 통쨰로 들어있는건 알겠고 이제 거기서 영화 하나 하나에 접근하고 싶어 -> response.results[0] 이러면 첫번째꺼가 나오네 !
+// 0번이 the Godfather 고 1번이 쇼생크 탈출이네. 좋아 그럼 이제 response.results를 배열 취급해도 된다는 소리겠지?
